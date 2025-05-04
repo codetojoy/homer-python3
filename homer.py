c0f420a225f505cf7547bed19e748d98dce22d77
Michael Easter <codetojoy@gmail.com>
2025-05-04 20:28:06 -0300

    incremental update Sun  4 May 2025 20:28:06 ADT

--- homer.py
+++ homer.py
@@ -1,4 +1,31 @@
 
+class Config:
+    def __init__(self):
+        self.main_template_file = 'resources/main.template.html'
+        self.link_template_file = 'resources/link.template.html'
+        self.output_file = 'index.html'
+
+class Token:
+    def __init__(self, token, new_value):
+        self.token = token
+        self.new_value = new_value
+
+class Stamper:
+    def stamp(self, template_file, tokens):
+        try:
+            with open(template_file, 'r') as file:
+                content = file.read()
+
+            for token in tokens:
+                placeholder = token.token
+                value = token.new_value
+                content = content.replace(placeholder, value)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        return content
+
 class LinkGroup:
     def __init__(self, name):
         self.name = name
@@ -7,12 +34,12 @@ class LinkGroup:
     def add_link(self, link):
         self.links.append(link) 
 
-    def to_string(self):
+    def to_string(self, config):
         result = "<div class=\"my-item\">\n"
         result += "<h3>" + self.name + "</h3>\n"
         result += "<div>\n"
         for link in self.links:
-            result += link.to_string()
+            result += link.to_string(config)
         result += "</div>\n"
         result += "</div>\n"
         return result
@@ -22,9 +49,11 @@ class Link:
         self.name = name
         self.url = url
 
-    def to_string(self):
-        template = '<a class="link" href="@URL">@NAME</a><br/>\n'
-        return template.replace("@URL", self.url).replace("@NAME", self.name)
+    def to_string(self, config):
+        template_file = config.link_template_file
+        tokens = [Token("@URL", self.url), Token("@NAME", self.name)]
+        content = Stamper().stamp(template_file, tokens)
+        return content 
 
 class HomerModel:
     def __init__(self):
@@ -40,11 +69,11 @@ class HomerModel:
         link = Link(name, url)
         self.current_link_group.add_link(link)
 
-    def to_string(self):
+    def to_string(self, config):
         template = '<div class="my-items">\n@LINK_GROUPS\n</div>\n'
         link_groups_content = ''
         for link_group in self.link_groups:
-            link_groups_content += link_group.to_string()
+            link_groups_content += link_group.to_string(config)
         return template.replace("@LINK_GROUPS", link_groups_content)
 
 class Consumer:
@@ -78,19 +107,15 @@ class Consumer:
         return self.homer_model
 
 class ContentWriter:
-    def __init__(self, template_file, output_file):
-        self.template_file = template_file
-        self.output_file = output_file
+    def __init__(self, config):
+        self.main_template_file = config.main_template_file
+        self.output_file = config.output_file
 
     def write(self, homer_model):
+        token = Token("@HOMER_MODEL", homer_model.to_string(config))
+        content = Stamper().stamp(self.main_template_file, [token])
+
         try:
-            with open(self.template_file, 'r') as file:
-                content = file.read()
-
-            placeholder = "@HOMER_MODEL"
-            value = homer_model.to_string()
-            content = content.replace(placeholder, value)
-
             with open(self.output_file, 'w') as file:
                 file.write(content)
 
@@ -100,6 +125,7 @@ class ContentWriter:
 # ----------------------------
 #
 
+config = Config()
 file = open('links.txt','r')
 
 consumer = Consumer()
@@ -107,7 +133,7 @@ consumer.process_file(file)
 
 homer_model = consumer.get_homer_model()
 
-content_writer = ContentWriter('resources/main.template.html', 'index.html')
+content_writer = ContentWriter(config)
 content_writer.write(homer_model)
 
 print("TRACER OK.")
